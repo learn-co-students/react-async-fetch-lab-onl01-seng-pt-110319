e23de6314a4954266fd84e1715a3452117e1817a
Aida Abdikulova <aabdikul@Aidas-MBP.fios-router.home>
2020-08-12 02:42:03 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,31 @@
 // create your App component here
+import React from 'react';
+
+export default class App extends React.Component {
+
+	constructor() {
+		super()
+		this.state = {
+			spacePeople: []
+		}
+	}
+
+	render() {
+		return (
+		<div>
+			{this.state.spacePeople.map(person => person.name)}
+		</div>
+		)
+	}
+
+	componentDidMount() {
+		fetch('http://api.open-notify.org/astros.json') 
+		.then(response => response.json())
+		.then(json => {
+			this.setState({
+				spacePeople: json.people
+			})
+		})
+	}
+
+}

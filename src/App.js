bb17b900caa033edaaacc5a2e9756ef8fc082246
github username <github email address>
2020-08-14 14:33:20 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, {Component} from 'react';
 
 class App extends Component {
-
+    constructor() {
+        super();
+        this.state = {
+            peopleInSpace: []
+        };
+    }
     componentDidMount() {
         const baseURL = 'http://api.open-notify.org/astros.json'
-        fetch(baseURL).then(res => res.json()).then(data => data)
+        fetch(baseURL).then(res => res.json()).then(data => this.setState({ peopleInSpace: data.people }))
     }
     render() {
-        return 
+        return (
+            <div>
+                {this.state.peopleInSpace.map((person) => <h1>{person.name}</h1>)}
+            </div>
+        )
     }
 }
 
